1cb1b7ca779ef2ced001c6ab93de1649c23911f2
Kirill Safelkin <k.safelkin@mail.ru>
2023-08-29 12:09:53 +0200

    Done

--- M_16_logging/mysite/myauth/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    agreement_accepted = models.BooleanField(default=False)
--- M_16_logging/mysite/shopapp/utils.py
+++
@@ -1,3 +0,0 @@
-
-def add_two_numbers(a, b):
-    return a + b
